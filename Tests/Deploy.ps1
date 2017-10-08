6c9616ecb141f5b6daa28d639934583b587cc0a9
ili101 <zoby101@gmail.com>
2017-10-08 14:42:34 +0300

    build
    build

--- Tests/Deploy.ps1
+++ Tests/Deploy.ps1
@@ -1,2 +1,7 @@
-﻿Get-ChildItem Env:
-Get-Variable
+﻿#Get-ChildItem Env:
+#Get-Variable
+"Name is $ModuleName"
+'Test 2'
+$PSScriptRoot
+'Test 3'
+[System.IO.Path]::GetFileNameWithoutExtension((Get-ChildItem -File -Filter *.psm1 -Name -Path "$PSScriptRoot\.."))
