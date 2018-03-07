2ca4a969e58fdf7c809e1abdbfdb7ea8990cf23b
Ili Metuky <ili@ntserv1.local>
2018-03-07 21:40:07 +0200

    build2

--- TestQ.psm1
+++ TestQ.psm1
@@ -18,4 +18,4 @@ class Base64z
 #Get-ChildItem -Path $PSScriptRoot | Unblock-File
 Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude Class.ps1 | Foreach-Object{ . $_.FullName }
 
-Export-ModuleMember -Variable Base64qV
+#Export-ModuleMember -Variable Base64qV
