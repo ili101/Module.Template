6989fd2b9d7d8ff77a2804061d6a88be249789e8
ili101 <zoby101@gmail.com>
2018-09-30 12:03:29 +0300

    pwsh test 5

--- Tests/AppVeyor.ps1
+++ Tests/AppVeyor.ps1
@@ -75,13 +75,17 @@ else # Finalize
     '[Progress] Finalizing'
     $Failure = $false
     # Upload results for test page
+    $Address = 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID
     Get-ChildItem -Path '.\TestResultsPS*.xml' | Foreach-Object {
-        $Address = 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID
         $Source = $_.FullName
         "[Output] Uploading Files: $Address, $Source"
-        [System.Net.WebClient]::new().UploadFile($Address, $Source)
+        $PSVersion = $_.Name.Replace('TestResults', '').Replace('.xml', '')
+        [xml]$Xml = Get-Content -Path $Source
+        Select-Xml -Xml $Xml -XPath '//test-case' | ForEach-Object {$_.Node.name = "$PSVersion " + $_.Node.name }
+        Invoke-RestMethod -Method Post -Uri $Address -Body $Xml.InnerText
+        #[System.Net.WebClient]::new().UploadFile($Address, $Source)
 
-        if (([Xml](Get-Content -Path $Source)).'test-results'.failures -ne '0')
+        if ($Xml.'test-results'.failures -ne '0')
         {
             $Failure = $true
         }
