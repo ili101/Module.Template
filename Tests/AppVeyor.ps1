4f7fe1b8c1b455e3a0891dbd2067ffbb8ab0fb7b
ili101 <zoby101@gmail.com>
2018-09-30 12:43:26 +0300

    pwsh test 7

--- Tests/AppVeyor.ps1
+++ Tests/AppVeyor.ps1
@@ -82,8 +82,9 @@ else # Finalize
         $PSVersion = $_.Name.Replace('TestResults', '').Replace('.xml', '')
         [xml]$Xml = Get-Content -Path $Source
         Select-Xml -Xml $Xml -XPath '//test-case' | ForEach-Object {$_.Node.name = "$PSVersion " + $_.Node.name }
-        Invoke-RestMethod -Method Post -Uri $Address -Body $Xml
-        #[System.Net.WebClient]::new().UploadFile($Address, $Source)
+        $Xml.OuterXml | Out-File -FilePath $Source
+        #Invoke-RestMethod -Method Post -Uri $Address -Body $Xml
+        [System.Net.WebClient]::new().UploadFile($Address, $Source)
 
         if ($Xml.'test-results'.failures -ne '0')
         {
