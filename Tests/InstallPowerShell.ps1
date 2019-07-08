0e84a722fc9261ad9a7c028184d430d2e618820d
ili101 <zoby101@gmail.com>
2019-07-08 16:10:21 +0300

    C

--- Tests/InstallPowerShell.ps1
+++ Tests/InstallPowerShell.ps1
@@ -8,14 +8,14 @@ $ErrorActionPreference = 'Stop'
 
 "Installing PowerShell Core..."
 "Downloading..."
-$MsiPath = Join-Path $env:TEMP "PowerShell-$PowerShellVersion-win-x64.msi"
-[System.Net.WebClient]::new().DownloadFile("https://github.com/PowerShell/PowerShell/releases/download/v$PowerShellVersion/PowerShell-$PowerShellVersion-win-x64.msi", $MsiPath)
+$MsiPath = Join-Path $env:TEMP "PowerShell-$Version-win-x64.msi"
+[System.Net.WebClient]::new().DownloadFile("https://github.com/PowerShell/PowerShell/releases/download/v$Version/PowerShell-$Version-win-x64.msi", $MsiPath)
 
 "Installing..."
 Start-Process 'msiexec.exe' -Wait -ArgumentList "/i $MsiPath /quiet"
 Remove-Item -Path $MsiPath
-$PowerShellFolder = $PowerShellVersion[0]
-if ($PowerShellVersion -like "*preview*") {
+$PowerShellFolder = $Version[0]
+if ($Version -like "*preview*") {
     $PowerShellFolder += '-preview'
 }
 $env:Path = "$env:ProgramFiles\PowerShell\$PowerShellFolder;$env:Path"
