a9fe8f9d5f38d3316a6f7ff3e129f92a49b9e67f
ili101 <zoby101@gmail.com>
2019-07-08 17:40:36 +0300

    Linux

--- Tests/Deploy.ps1
+++ Tests/Deploy.ps1
@@ -11,85 +11,79 @@ Param
 
     [Switch]$PowerShellGallery,
 
-    [Switch]$AppVeyorZip
+    [Switch]$AppVeyorZip,
+
+    [validateset('Windows', 'Linux')]
+    [String]$RunOnlyOn
 )
 
 $ErrorActionPreferenceOrg = $ErrorActionPreference
 $ErrorActionPreference = 'Stop'
-try
-{
+
+if ($RunOnlyOn -eq 'Windows' -and !$IsWindows -or
+    $RunOnlyOn -eq 'Linux' -and !$IsLinux) {
+    "Skipping deploy as script set to run on $RunOnlyOn"
+    break
+}
+
+try {
     # Get Script Root
-    if ($PSScriptRoot)
-    {
+    if ($PSScriptRoot) {
         $ScriptRoot = $PSScriptRoot
     }
-    elseif ($psISE.CurrentFile.IsUntitled -eq $false)
-    {
+    elseif ($psISE.CurrentFile.IsUntitled -eq $false) {
         $ScriptRoot = Split-Path -Path $psISE.CurrentFile.FullPath
     }
-    elseif ($null -ne $psEditor.GetEditorContext().CurrentFile.Path -and $psEditor.GetEditorContext().CurrentFile.Path -notlike 'untitled:*')
-    {
+    elseif ($null -ne $psEditor.GetEditorContext().CurrentFile.Path -and $psEditor.GetEditorContext().CurrentFile.Path -notlike 'untitled:*') {
         $ScriptRoot = Split-Path -Path $psEditor.GetEditorContext().CurrentFile.Path
     }
-    else
-    {
+    else {
         $ScriptRoot = '.'
     }
 
     # Get Module Info
-    if (!$ModuleName)
-    {
+    if (!$ModuleName) {
         $ModuleName = [System.IO.Path]::GetFileNameWithoutExtension((Get-ChildItem -File -Filter *.psm1 -Name -Path "$ScriptRoot\.."))
     }
     $VersionLocal = ((Get-Module -Name $ModuleName -ListAvailable).Version | Measure-Object -Maximum).Maximum
     "[Progress] Deploy Script Start for Module: $ModuleName, Version: $VersionLocal"
 
-    if ((!$Env:APPVEYOR -or $Env:APPVEYOR_REPO_BRANCH -eq 'master') -and $PowerShellGallery)
-    {
-        try
-        {
+    if ((!$Env:APPVEYOR -or $Env:APPVEYOR_REPO_BRANCH -eq 'master') -and $PowerShellGallery) {
+        try {
             $VersionGallery = (Find-Module -Name $ModuleName -ErrorAction Stop).Version
         }
-        catch
-        {
-            if ($_.Exception.Message -notlike 'No match was found for the specified search criteria*' -or !$Force)
-            {
+        catch {
+            if ($_.Exception.Message -notlike 'No match was found for the specified search criteria*' -or !$Force) {
                 throw $_
             }
         }
 
         "[Output] PowerShellGallery. $ModuleName, VersionGallery: $VersionGallery, VersionLocal: $VersionLocal"
-        if ($VersionGallery -lt $VersionLocal -or $Force)
-        {
-            if (!$NugetApiKey)
-            {
+        if ($VersionGallery -lt $VersionLocal -or $Force) {
+            if (!$NugetApiKey) {
                 $NugetApiKey = $Env:NugetApiKey
             }
             "[Output] PowerShellGallery. Deploying $ModuleName version $VersionLocal"
             Publish-Module -Name $ModuleName -NuGetApiKey $NugetApiKey -RequiredVersion $VersionLocal
         }
-        else
-        {
+        else {
             '[Output] PowerShellGallery Deploy Skipped (Version Check)'
         }
     }
-    if ($Env:APPVEYOR -and $AppVeyorZip)
-    {
+    if ($Env:APPVEYOR -and $AppVeyorZip) {
         $ZipFileName = "{0} {1} {2} {3:yyyy-MM-dd HH-mm-ss}.zip" -f $ModuleName, $VersionLocal, $Env:APPVEYOR_REPO_BRANCH, (Get-Date)
         $ZipFileFullPath = Join-Path -Path $ScriptRoot -ChildPath $ZipFileName
-         "[Output] AppVeyorZip. $ModuleName, ZipFileName: $ZipFileName"
+        "[Output] AppVeyorZip. $ModuleName, ZipFileName: $ZipFileName"
         $ModulePath = (Get-Module -Name $ModuleName -ListAvailable).ModuleBase | Split-Path
         #Compress-Archive -Path $ModulePath -DestinationPath $ZipFileFullPath
         [System.IO.Compression.ZipFile]::CreateFromDirectory($ModulePath, $ZipFileFullPath, [System.IO.Compression.CompressionLevel]::Optimal, $true)
         Push-AppveyorArtifact $ZipFileFullPath -DeploymentName $ModuleName
     }
 }
-catch
-{
+catch {
     throw $_
 }
-finally
-{
+finally {
     $ErrorActionPreference = $ErrorActionPreferenceOrg
 }
 '[Progress] Deploy Ended'
